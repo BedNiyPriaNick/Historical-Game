5f8c89a1ea371e2d0f0a44775a08a16ab7059d62
BedNiyPriaNick <egbra402@gmail.com>
2023-01-01 17:17:24 +0100

    BasicOfTycoons

---
+++ Assets/C#/GetMoney.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetMoney : MonoBehaviour
+{
+    [SerializeField] private float Money = 0, MoneyAdded = 400, Timer;
+    [SerializeField] private Text MoneyText, TimerText;
+    private int StartTimer = 0;
+    private float TimeReset;
+
+    private void Start()
+    {
+        TimeReset = Timer;
+    }
+
+    private void Update()
+    {
+        MoneyText.text = Money.ToString("C2");
+        TimerText.text = Timer.ToString("0.0");
+
+        if(StartTimer >= 1)
+        {
+            Timer -= 1 * Time.deltaTime;
+        }
+
+        if(Timer < -0)
+        {
+            Money += MoneyAdded;
+            Timer = TimeReset;
+            StartTimer = 0;
+        }
+    }
+
+    public void StartBtnClicked()
+    {
+        StartTimer = 1;
+    }
+}
